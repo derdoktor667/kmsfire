75950b6a43cf273cf12d3156e139111c3a2c9bd3
Wastl Kraus <derdoktor667@gmail.com>
2015-11-08 03:57:37 +0100

    Changes to be committed: 	modified:   install-kmsfire.sh 	modified:   kmsfire

--- install-kmsfire.sh
+++ install-kmsfire.sh
@@ -1,11 +1,17 @@
-#!/bin/bash1
+#!/bin/sh
 
 ServiceDir=/etc/init.d/
 InitScriptName=$ServiceDir/kmsfire
 Package=./kmsfire.tar.gz
+DeamonName=kmsfire
 
 cp ./kmsfire $InitScriptName
 chmod a+x $InitScriptName
 
-tar -xvf $Package -C /opt/kmsfire
+mkdir /opt/$DeamonName
+tar -xvf $Package -C /opt
 
+ln -s $ServiceDir/kmsfire /etc/rc.d/rc3.d/S45kmsfire
+
+$ServiceDir/kmsfire start
+$ServiceDir/kmsfire status
