0c278ecad418025f24df46f74a4840b0ed6afc12
Wastl Kraus <derdoktor667@gmail.com>
2015-11-08 03:34:28 +0100

    first things first

--- install-kmsfire.sh
+++ install-kmsfire.sh
@@ -1,2 +1,11 @@
-#!/bin/bash
+#!/bin/bash1
+
+ServiceDir=/etc/init.d/
+InitScriptName=$ServiceDir/kmsfire
+Package=./kmsfire.tar.gz
+
+cp ./kmsfire $InitScriptName
+chmod a+x $InitScriptName
+
+tar -xvf $Package -C /opt/kmsfire
 
